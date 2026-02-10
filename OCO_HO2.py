476e395a450bf3d867cc3d690a85c884038fc9fe
Daniel Oco <ocodaniel13@gmail.com>
2026-02-10 22:10:09 +0800

    Add user profile GUI using Tkinter
    Create a Tkinter window displaying a user profile with name, age, course, birthday, and motto.

---
+++ OCO_HO2.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+window = tk.Tk()
+
+window.title("Dan's Profile")
+window.geometry("600x600")
+window.resizable(False, True)
+window.configure(bg="beige", cursor="hand2")
+
+label = tk.Label(window, text="MY PROFILE", font=("Poppins",35, "italic"),fg="pink", bg="beige", anchor="center")
+label.pack(padx=5, pady=20)
+
+label = tk.Label(window, text="name : Daniel oco ", font=("serif", 12, "italic"),fg="pink", bg="beige")
+label.pack(anchor="w")
+
+label = tk.Label(window, text="Age : 19", font=("serif", 12, "italic"), fg="pink", bg="beige")
+label.pack(anchor="w")
+
+label = tk.Label(window, text="Course : BSIT", font=("serif", 12, "italic"), fg="pink",bg="beige")
+label.pack(anchor="w")
+
+label = tk.Label(window, text="Birth Day : December 1, 2006", font=("serif", 12, "Italic"), fg="pink", bg="beige")
+label.pack(anchor="w")
+
+label = tk.Label(window, text="Motto: Continue your dreams keep sleeping.",font=("serif",12,"Italic"),fg="pink",bg="beige")
+label.pack(anchor="w")
+
+window.mainloop()
